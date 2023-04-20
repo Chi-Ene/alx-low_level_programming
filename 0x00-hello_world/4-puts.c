9aab4372765be0d235be605fe11ba1773cb6c6ef
Chi-Ene <chiomaenebeli@gmail.com>
2023-04-20 01:52:05 -0700

    hello-world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - A program that prints a line with puts function
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	PUTSputs("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
