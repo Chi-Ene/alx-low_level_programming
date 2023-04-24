07fd625ed65970143c22c5f0fa761d54c311d7ec
Chi-Ene <chiomaenebeli@gmail.com>
2023-04-23 18:47:24 -0700

    hello-world correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - a program that prints the size of various computers with printf function
+ * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	char a;
@@ -11,10 +11,12 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %lu byte(s)\n",(unsigned int)sizeof(a));
-	printf("Size of a int: %lu byte(s)\n", (unsigned int)sizeof(b));
+	printf("Size of an int: %lu byte(s)\n", (unsigned int)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned int)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned int)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned int)sizeof(e));
+
 	return (0);
 }
